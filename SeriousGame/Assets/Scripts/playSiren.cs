b32232148dbb99ad3fd34ba2b08c99202e57fe26
Thibault <thibault3470@outlook.fr>
2023-10-05 22:52:22 +0200

    update maison

---
+++ SeriousGame/Assets/Scripts/playSiren.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSiren : MonoBehaviour
+{
+    public AudioSource siren;
+    void Start()
+    {
+        siren.Play();
+    }
+
+    void Update()
+    {
+
+    }
+}
