c3b75019c6970349620357032158b4e4bfebd1cc
MarieBo2708 <marie.n.bocquelet@gmail.com>
2023-10-18 13:05:13 +0200

    Suite enigme benoit phone

--- SeriousGame/Assets/Scripts/takePhone.cs
+++ SeriousGame/Assets/Scripts/takePhone.cs
@@ -6,27 +6,32 @@ public class takePhone : MonoBehaviour
 {
     public GameObject player;
     public Canvas canvas;
-    /*PlayerMovement mvtJoueur;*/
     // Start is called before the first frame update
     void Start()
     {
         canvas.gameObject.SetActive(false);
-        /*mvtJoueur = player.GetComponent<PlayerMovement>();*/
         
     }
 
     private void OnTriggerEnter(Collider other){
         if(other.tag == "Player"){
             canvas.gameObject.SetActive(true);
-            /*mvtJoueur.enabled = false;*/
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         canvas.gameObject.SetActive(false);
-        /*mvtJoueur.enabled = true;*/
-        // Start is called before the first frame update
+       
+    }
+
+    private void OnTriggerStay(Collider other){
+        if(other.tag == "Player"){
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("F key was pressed.");
+            }
+        }
     }
 
 }
